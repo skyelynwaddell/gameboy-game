276711a19b44aa446ec22dfecdec902aa861e135
Skye <105599773+skyelynwaddell@users.noreply.github.com>
2023-07-10 01:45:02 -0600

    added scrolling backgrounds

--- test game/main.c
+++ test game/main.c
@@ -2,17 +2,25 @@
 #include <gb/gb.h>
 #include <stdio.h>
 #include "./sprites/player.c"
+#include "./maps/home_town_tiles.c"
+#include "./maps/home_town.c"
 
 void main()
 {
     //game variables
+    uint8_t bkgSpeed = 1;
+    uint8_t playerSpeed = 0.10;
     uint8_t spriteIndex = 0;
     uint8_t playerX = 80;
     uint8_t playerY = 70;
     uint8_t mButtons;
 
     //debug msg
-    printf("ver2ion is the best");
+    printf("pocket monsters 2");
+
+    set_bkg_data(0,12,homeTownTiles);
+    set_bkg_tiles(0,0,40,18,homeTownMap);
+    SHOW_BKG;
 
     //player top left
     set_sprite_data(0,1,player);
@@ -34,44 +42,64 @@ void main()
     set_sprite_tile(3,6);
     move_sprite(3,playerX+8,playerY+8);
 
-
-    SHOW_SPRITES;
+    SHOW_SPRITES; // gb draw sprites function
+    DISPLAY_ON;
 
     //main game loop
     while(1)
-    {
+    {   
         mButtons = joypad();
 
         switch(joypad()){
             case J_LEFT:
-            scroll_sprite(0,-1,0);
-            scroll_sprite(1,-1,0);
-            scroll_sprite(2,-1,0);
-            scroll_sprite(3,-1,0);
+            scroll_sprite(0,-playerSpeed,0);
+            scroll_sprite(1,-playerSpeed,0);
+            scroll_sprite(2,-playerSpeed,0);
+            scroll_sprite(3,-playerSpeed,0);
+            scroll_bkg(-bkgSpeed,0);
             break;
 
             case J_RIGHT:
-            scroll_sprite(0,1,0);
-            scroll_sprite(1,1,0);
-            scroll_sprite(2,1,0);
-            scroll_sprite(3,1,0);
+            scroll_sprite(0,playerSpeed,0);
+            scroll_sprite(1,playerSpeed,0);
+            scroll_sprite(2,playerSpeed,0);
+            scroll_sprite(3,playerSpeed,0);
+            scroll_bkg(bkgSpeed,0);
             break;
 
             case J_UP:
-            scroll_sprite(0,0,-1);
-            scroll_sprite(1,0,-1);
-            scroll_sprite(2,0,-1);
-            scroll_sprite(3,0,-1);
+            scroll_sprite(0,0,-playerSpeed);
+            scroll_sprite(1,0,-playerSpeed);
+            scroll_sprite(2,0,-playerSpeed);
+            scroll_sprite(3,0,-playerSpeed);
+            scroll_bkg(0,-bkgSpeed);
             break;
 
             case J_DOWN:
-            scroll_sprite(0,0,1);
-            scroll_sprite(1,0,1);
-            scroll_sprite(2,0,1);
-            scroll_sprite(3,0,1);
+            scroll_sprite(0,0,playerSpeed);
+            scroll_sprite(1,0,playerSpeed);
+            scroll_sprite(2,0,playerSpeed);
+            scroll_sprite(3,0,playerSpeed);
+            scroll_bkg(0,bkgSpeed);
+            break;
+
+            case J_A:
+            //printf("A");
+            break;
+
+            case J_B:
+            //printf("B");
+            break;
+
+            case J_SELECT:
+            //printf("select");
+            break;
+
+            case J_START:
+            //printf("start");
             break;
         }
-        delay(15);
+        delay(25);
 
     }
 }
