12707b03e410a24de5b5513171b86d9dde5ab7b5
Skye <105599773+skyelynwaddell@users.noreply.github.com>
2023-07-09 23:00:13 -0600

    added gbtd and gbmd for gbdk2020

--- test game/main.c
+++ test game/main.c
@@ -1,7 +1,18 @@
+//gameboy game by skye waddell <3
 #include <gb/gb.h>
 #include <stdio.h>
 
 void main()
 {
+    uint8_t mButtons;
     printf("ver2ion is the best");
+
+    while(1)
+    {
+        mButtons = joypad();
+        if(mButtons & J_A)
+            printf("A ");
+        
+        while(mButtons == joypad());
+    }
 }
